a93ddd80c8a59ca981ef4d25fa7ca310e8e94821
yujia21 <odonut@gmail.com>
2016-12-02 20:06:21 +0100

    Updates major updates!!

--- Bootstrap.py
+++ Bootstrap.py
@@ -1,6 +1,8 @@
 
 # coding: utf-8
 
+# In[118]:
+
 # Import libraries
 import pandas as pd
 import numpy as np
@@ -16,12 +18,14 @@ evaluation=True # True if want to evaluate error of machine learning algo
 submit=True # True if want to replace submission file by new values
 
 
+# In[119]:
 
 #Import data if not already done
 if (not imported_data) : 
     import ImportData
 
 
+# In[5]:
 
 # BEGINNING OF LEARNING CODE
 # Read submission data
@@ -36,31 +40,114 @@ for x in ass_list :
     data[x] = np.load('data/'+x+'.npy')
 
 
+# In[ ]:
+
+#Adding features
+# Can choose to add more features if desired! 
+
+# add_features() changes structure of data[x] where x is a service name :
+# date (timestamp) / number of calls / day of week / month / time
+# time is from 0 to 47 (hour*2+minutes/30)
+def add_features(): 
+    print("Features added for : ")
+    for x in ass_list :
+        # Initialize two columns of zero
+        n=np.shape(data[x])[0]
+        z=np.zeros((n,3),int) 
+        
+        # Append zero column twice
+        data[x]=np.append(data[x],z,axis=1)
+
+        # Addding Columns : Day of week / Month
+        for i in range(n) :
+            data[x][i,2]=pd.to_datetime(data[x][i,0]).weekday()
+            data[x][i,3]=data[x][i,0].month
+            data[x][i,4]=int((2*data[x][i,0].hour)+(data[x][i,0].minute/30))
+            
+        if (x!='Prestataires') : 
+            print(x, end=", ")
+        else : 
+            print(x)
+        # TODO IMPROVE
+        # - possibly much faster if adding features in dataframe
+        # - Label Binarizer - change magnitude of day of week, month, hour
+        #                     to 7, 31, 48 columns of 0 and 1s respectively
+        
+# del_features deletes everything from column 2
+def del_features():
+    for x in ass_list :
+        data[x]=np.delete(data[x],np.s_[2:np.shape(data[x])[1]],1)
+        
+del_features()
+add_features()
+#print(data[ass_list[0]][::1000,:])
+
+
+# In[ ]:
+
+df={}
+for x in ass_list : 
+    df[x]=pd.DataFrame.from_dict(data[x])
+    df[x].columns=['DATETIME','CALLS','DAY','MONTH','TIME']
+
+
+# In[31]:
 
 # data is ambient dictionary in this script
 
-# predict takes in "dates" which is a list of dates (continuous)
-# predict returns a dictionary predicted where:
+# predict_single_week takes in "dates" which is a list of timestamps (continuous)
+# Assume dates given in groups of weeks, take minimum of dates. 
+# predict_single_week returns a dictionary predicted where:
 # keys = services, values = numpy arrays (TimeStamp, Calls Received)
 
-# predict contains a machine learning algorithm that does : 
-# 1. Preprocessing - feature reduction + engineering features if necessary
-# 2. Learning - regression, random forest, neural network, etc.
-
-def predict(dates) : 
+def predict_single_week(dates) : 
     #Initialize and clear dictionary to return
     predicted={}
     predicted.clear()
-
+    
+    # X and Y restocks df as information needed to predict and calls, until min date
+    
+    # Assume dates given in groups of weeks, take minimum of dates. 
+    min_date = min(dates)
+    X={}
+    Y={}
+    est={}
+    
+    for x in ass_list : 
+        X[x]=df[x][['DAY','MONTH','TIME']][df[x]['DATETIME']<pd.to_datetime(min_date)].as_matrix()
+        Y[x]=df[x]['CALLS'][df[x]['DATETIME']<pd.to_datetime(min_date)].as_matrix()
+    
     # TODO : CODE HERE
     
+    #from sklearn.ensemble import GradientBoostingRegressor
+    
+    # Creating each predictor
+    #for x in ass_list : 
+    #    clf=GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=0, loss='ls')
+    #    est[x] = clf.fit(X[x], Y[x])
     
     
+    # Creating X_test from dates that has all information needed
+    #X_test=np.zeros((len(dates),3),int)
+
+    #for i in range(len(dates)) :
+    #    X_test[i,0]=dates[i].weekday()
+    #    X_test[i,1]=dates[i].month
+    #    X_test[i,2]=int((2*dates[i].hour)+(dates[i].minute/30))
+
+    # Calculating y_test for each predictor
+    #y_test={}
+    #for x in ass_list : 
+        # Round up always
+        #y_test[x]=np.ceil(est[x].predict(X_test)).astype(int)
+        # Make sure all positive
+        #y_test[x]=y_test[x]*(y_test[x]>0)
+        #predicted[x]=np.append(np.reshape(dates,(len(dates),1)),np.reshape(y_test[x],(len(dates),1)),axis=1)
     
     return predicted
 
 
-
+# In[115]:
 
 # Function dates_to_datetime creates datetime stamps at every half hour from a list of date
 def dates_to_datetime(dates) : 
@@ -83,6 +170,37 @@ def dates_to_datetime(dates) :
             datetimes.append(datetime.strptime(datetime_entry, "%Y-%m-%d %H:%M:%S.%f"),)
     return datetimes
 
+# Split list of dates into list of (weeks=list of datetimes)
+def dates_to_weeks(dates):
+    weeks=[]
+    week=[]
+    current_wk=0
+
+    for date in dates : 
+        if (len(week)==0) :
+            week.append(datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f"))
+
+        elif (len(week)==14*48):
+            weeks.append(week)
+            week=[]
+
+        else : 
+            current_wk = week[-1].isocalendar()[:2]
+
+            if (current_wk[1]==52) :
+                next_wk=(current_wk[0]+1,1)
+            else : 
+                next_wk=(current_wk[0],current_wk[1]+1)
+
+
+            if ((datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f").isocalendar()[:2]==current_wk)or(datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f").isocalendar()[:2]==next_wk)) :
+                week.append(datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f"))
+            else :
+                weeks.append(week)
+                week=[]
+    return weeks
+
+
 # Error function as defined in project
 def linex(y_true,y_pred) : 
     a=0.1
@@ -91,7 +209,28 @@ def linex(y_true,y_pred) :
     return np.exp(a*diff)-a*diff-np.ones(len(y_true))
 
 
+# In[ ]:
 
+# Predict for each week in list, append all predicted
+def predict(dates) : 
+    weeks=dates_to_weeks(dates)
+    predicted={}
+
+    for week in weeks : 
+        print("Predicting : ", end='')
+        print(week[-1].isocalendar()[:2])
+        predicted_week=predict_single_week(week)
+
+        for x in ass_list : 
+            if (x in predicted) :
+                predicted[x]=np.concatenate((predicted[x], predicted_week[x]), axis=0)
+            else :
+                predicted[x]=predicted_week[x]
+
+    return predicted
+
+
+# In[ ]:
 
 #EVALUATION
 if (evaluation) : 
@@ -138,7 +277,33 @@ if (evaluation) :
     print(err_tot)
 
 
+# In[ ]:
 
+# Visualization of original vs predicted for evaluation range
+if (evaluation):
+    df2={}
+    mean={}
+
+    for x in ass_list : 
+        print(x)
+        #Original is green
+        df2[x]=df[x][['DATETIME','CALLS']][df[x]['DATETIME'].isin(predicted[x][:,0])].as_matrix()
+        plt.plot(df2[x][:,0],df2[x][:,1],'g')
+
+        #Predicted is red
+        plt.plot(predicted[x][:,0],predicted[x][:,1],'r')
+
+        #Mean is blue
+        #mean[x]=[]
+        #for i in range(48) : # For each hour
+        #     mean[x]=np.append(mean[x],(df[x]['CALLS'][df[x]['TIME']==i].mean()))
+        #mean[x]=np.tile(mean[x],len(predicted[x][:,0])/48)
+
+        #plt.plot(predicted[x][:,0],mean[x],'b')
+        plt.show()
+
+
+# In[116]:
 
 # UPDATE SUBMISSIONS.TXT
 # ASS_ASSIGNMENT : because not every service appears at any time, 
@@ -146,12 +311,10 @@ if (evaluation) :
 # we cannot construct new dataframe directly from predicted
 
 if (submit) : 
-    # List of dates in submission.txt file
-    dates=[]
-    for date in submission['DATE'].unique() : 
-        dates.append(datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f"))
+    # List of dates in submission.txt
+    dates=submission['DATE'].unique()
     
-    # Call predict function
+    # Call predict function on weeks
     predicted=predict(dates)
     
     #Rewrite submissions dataframe
@@ -161,10 +324,11 @@ if (submit) :
             for row in predicted[x] : # row=[date,calls]
                 # Convert timestamp to yyyy-mm-dd hh:mm:ss.xxx
                 date = row[0].strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+                # Replace values in submission dsataframe
+                #better way to loop through this?
                 submission.loc[(submission['DATE']==date)&(submission['ASS_ASSIGNMENT']==x), ['prediction']]=row[1]
 
-                #better way to loop through this?
-    
+    # EXPORT to submission.txt 
     fh = open("submission1.txt","w")
     fh.write("DATE\tASS_ASSIGNMENT\tprediction")
     for index, row in submission.iterrows(): 
